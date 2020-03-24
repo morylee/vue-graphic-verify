e3272193830188e696c8da3af36275b9fdb1c2f4
mory.lee <lichenhui@cloudcrowd.com.cn>
2020-03-24 18:47:18 +0800

    优化拖拽验证手机端交互

--- src/lib/shared/dragGuideBar.vue
+++ src/lib/shared/dragGuideBar.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="verifyGuideBar" @touchmove="movingDrag" @touchend="finishDrag" @mousemove="movingDrag" @mouseup="finishDrag">
-    <span ref="dragBar" class="dragBar" :style="{left: '5px'}" @touchstart="startDrag" @mousedown="startDrag"></span>
-    <span class="progressBar"></span>
+  <div class="verifyGuideBar">
+    <div @touchmove.stop.prevent="movingDrag" @touchend.stop.prevent="finishDrag" @mousemove="movingDrag" @mouseup="finishDrag">
+      <span ref="dragBar" class="dragBar" :style="{left: '5px'}" @touchstart.stop.prevent="startDrag" @mousedown="startDrag"></span>
+      <span class="progressBar"></span>
+    </div>
     <img class="freshBtn" src="../images/refresh.png" @click.prevent="fresh()" title="刷新">
   </div>
 </template>
